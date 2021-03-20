7837cd645689ed46bc1daa7f937e93e58a27f1b4
chenjie <chenjie@meari.com.cn>
2021-03-20 10:47:19 +0800

    伪实现recvmg和__cmsg_nxthdr for liteos, -DLITEOS

--- usrsctplib/user_recv_thread.c
+++ usrsctplib/user_recv_thread.c
@@ -56,6 +56,36 @@
 #endif
 #endif
 #endif
+
+#ifdef LITEOS
+static ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags);
+{
+
+	return 0;
+}
+
+static struct cmsghdr *
+__cmsg_nxthdr (struct msghdr *mhdr, struct cmsghdr *cmsg)
+{
+  if ((size_t) cmsg->cmsg_len < sizeof (struct cmsghdr))
+    /* The kernel header does this so there may be a reason.  */
+    return NULL;
+
+  cmsg = (struct cmsghdr *) ((unsigned char *) cmsg
+			     + CMSG_ALIGN (cmsg->cmsg_len));
+  if ((unsigned char *) (cmsg + 1) > ((unsigned char *) mhdr->msg_control
+				      + mhdr->msg_controllen)
+      || ((unsigned char *) cmsg + CMSG_ALIGN (cmsg->cmsg_len)
+	  > ((unsigned char *) mhdr->msg_control + mhdr->msg_controllen)))
+    /* No more entries.  */
+    return NULL;
+  return cmsg;
+}
+
+
+#endif //LITEOS
+
+
 #if defined(__APPLE__) || defined(__DragonFly__) || defined(__FreeBSD__)
 #include <net/route.h>
 #endif
